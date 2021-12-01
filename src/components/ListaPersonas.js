a8322e16559d341863616ce9daae4fd3bea1564b
Nicolay Moreno <nicolay.mherrera@gmail.com>
2021-11-30 21:06:43 -0500

    Alineando lista de personas con bootstrap

--- src/components/ListaPersonas.js
+++ src/components/ListaPersonas.js
@@ -2,32 +2,30 @@ import React from 'react';
 
 export const ListaPersonas = ({ persona, handleDelete, handleEdit }) => {
     return (
-        <div className=''>
-            <li className='text-capitalize list-group-item d-flex justify-content-between align-items-center bg-light'>
-                {persona.nombres} {persona.apellidos}
-                <div className='d-flex'>
-                    <button
-                        className='btn btn-danger mx-1'
-                        onClick={() => {
-                            handleDelete(persona.id);
-                        }}
-                    >
-                        Delete
-                    </button>
-                    <button
-                        className='btn btn-warning'
-                        onClick={() =>
-                            handleEdit(
-                                persona.id,
-                                persona.nombres,
-                                persona.apellidos,
-                            )
-                        }
-                    >
-                        Editar
-                    </button>
-                </div>
-            </li>
-        </div>
+        <li className='text-capitalize list-group-item d-flex justify-content-between align-items-center bg-light px-2'>
+            {persona.nombres} {persona.apellidos}
+            <div className='d-flex'>
+                <button
+                    className='btn btn-danger mx-1'
+                    onClick={() => {
+                        handleDelete(persona.id);
+                    }}
+                >
+                    Delete
+                </button>
+                <button
+                    className='btn btn-warning'
+                    onClick={() =>
+                        handleEdit(
+                            persona.id,
+                            persona.nombres,
+                            persona.apellidos,
+                        )
+                    }
+                >
+                    Editar
+                </button>
+            </div>
+        </li>
     );
 };
